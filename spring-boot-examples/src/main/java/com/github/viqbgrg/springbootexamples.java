db42a5311f3816cefd01ee1f379d5d119f7bd33e
viqbgrg <viqbgrg@gmail.com>
2020-09-01 17:52:41 +0800

    'spring-boot'

---
+++ spring-boot-examples/src/main/java/com/github/viqbgrg/springbootexamples.java
@@ -0,0 +1,5 @@
+package com.github.viqbgrg;
+
+
+public class springbootexamples {
+}
---
+++ src/main/java/com/github/viqbgrg/javaframeworkexamples/JavaFrameworkExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.javaframeworkexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFrameworkExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFrameworkExamplesApplication.class, args);
+    }
+
+}
