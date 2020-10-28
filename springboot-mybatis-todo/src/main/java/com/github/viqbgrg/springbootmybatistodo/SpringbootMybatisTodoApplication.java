51b4a8000520ae181d11e80eda501e025cdefb2c
viqbgrg <viqbgrg@gmail.com>
2020-10-28 12:30:35 +0800

    springboot mybatis tdd 项目初始化

---
+++ springboot-mybatis-todo/src/main/java/com/github/viqbgrg/springbootmybatistodo/SpringbootMybatisTodoApplication.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.springbootmybatistodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisTodoApplication.class, args);
+    }
+
+}
