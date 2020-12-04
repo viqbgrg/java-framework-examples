e78d3b710f489b450c9d1899bd1feebb897c3b52
viqbgrg <viqbgrg@gmail.com>
2020-12-04 11:02:19 +0800

    boot-features-testing

---
+++ spring-boot-demo/boot-features-testing/src/main/java/com/github/viqbgrg/springboottesting/TestingApplication.java
@@ -0,0 +1,11 @@
+package com.github.viqbgrg.springboottesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestingApplication.class, args);
+    }
+}
---
+++ spring-boot-demo/boot-features-testing/src/test/java/com/github/viqbgrg/springboottesting/TestingApplicationTest.java
@@ -0,0 +1,12 @@
+package com.github.viqbgrg.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
