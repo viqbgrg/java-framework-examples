9be75f0cb197b2bfd3e964bfee87ded123ff9208
viqbgrg <viqbgrg@gmail.com>
2020-12-22 11:30:21 +0800

    spring action demo

---
+++ springinaction/src/main/java/com/github/viqbgrg/springinaction/SpringinactionApplication.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.springinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringinactionApplication.class, args);
+    }
+
+}
---
+++ springinaction/src/test/java/com/github/viqbgrg/springinaction/SpringinactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.springinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
