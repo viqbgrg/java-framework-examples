eea588ad8eab0f4c6121c3569a5c14ad4f1dd802
viqbgrg <viqbgrg@gmail.com>
2021-01-08 19:17:39 +0800

    gzip

--- spring-boot-demo/boot-features-developing-web-applications/src/main/java/com/github/viqbgrg/webapplication/controller/GZipController.java
+++ spring-boot-demo/boot-features-developing-web-applications/src/main/java/com/github/viqbgrg/webapplication/controller/GZipController.java
@@ -1,20 +1,43 @@
 package com.github.viqbgrg.webapplication.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
+
 @RestController
 @RequestMapping
 public class GZipController {
-    @GetMapping("/gzip")
-    public ResponseEntity<User> gzip() {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping("/nogzip")
+    public ResponseEntity<User> nogzip(HttpServletResponse response) throws IOException {
         User user = new User();
         user.setUsername("xiaoming");
         user.setEmail("123@qq.com");
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/gzip")
+    public void gzip(HttpServletResponse response) throws IOException {
+        User user = new User();
+        user.setUsername("xiaoming");
+        user.setEmail("123@qq.com");
+        response.addHeader("Content-Encoding", "gzip");
+        response.addHeader("Content-Type", "application/json");
+        OutputStream out = response.getOutputStream();
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(out);
+        gzipOutputStream.write(objectMapper.writeValueAsBytes(user));
+        gzipOutputStream.close();
     }
 
     @Data
@@ -23,3 +46,4 @@ public class GZipController {
         private String email;
     }
 }
+
