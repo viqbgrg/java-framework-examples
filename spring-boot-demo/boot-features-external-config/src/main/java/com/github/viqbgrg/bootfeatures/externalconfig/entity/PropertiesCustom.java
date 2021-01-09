940920d09e473644a5439f9ea0099546d77af236
viqbgrg <viqbgrg@gmail.com>
2021-01-10 01:31:09 +0800

    @TestPropertySource("classpath:test.yml")

--- spring-boot-demo/boot-features-external-config/src/main/java/com/github/viqbgrg/bootfeatures/externalconfig/entity/PropertiesCustom.java
+++ spring-boot-demo/boot-features-external-config/src/main/java/com/github/viqbgrg/bootfeatures/externalconfig/entity/PropertiesCustom.java
@@ -3,10 +3,8 @@ package com.github.viqbgrg.bootfeatures.externalconfig.entity;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:test.yml")
 public class PropertiesCustom {
 
     @Bean
