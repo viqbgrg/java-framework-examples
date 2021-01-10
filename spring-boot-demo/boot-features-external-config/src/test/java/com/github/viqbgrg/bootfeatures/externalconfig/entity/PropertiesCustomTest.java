f69f2b68abf1ec8844e20af9837757b2b14e2573
viqbgrg <viqbgrg@gmail.com>
2021-01-10 21:44:28 +0800

    @TestPropertySource 不能获取yml的值

--- spring-boot-demo/boot-features-external-config/src/test/java/com/github/viqbgrg/bootfeatures/externalconfig/entity/PropertiesCustomTest.java
+++ spring-boot-demo/boot-features-external-config/src/test/java/com/github/viqbgrg/bootfeatures/externalconfig/entity/PropertiesCustomTest.java
@@ -3,19 +3,26 @@ package com.github.viqbgrg.bootfeatures.externalconfig.entity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@TestPropertySource("classpath:test.yml")
+@ActiveProfiles("test")
 class PropertiesCustomTest {
 
     @Autowired
     private TestBean testBean;
 
+
+    @Autowired
+    private Environment environment;
+
+
     @Test
     void getTestBean() {
-        assertThat(testBean).isNotNull();
+        assertThat(environment).isNotNull();
+        assertThat(testBean.getName()).isNotEmpty();
     }
 }
