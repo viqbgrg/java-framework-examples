c2b3342c109dfebe06f0d22ab8fbeb0ffcc4b37d
viqbgrg <viqbgrg@gmail.com>
2021-01-17 21:35:11 +0800

    jsonArray

--- jackson-demo/src/main/java/com/github/viqbgrg/jackson/JSONStringTest.java
+++ jackson-demo/src/main/java/com/github/viqbgrg/jackson/JSONStringTest.java
@@ -1,12 +1,11 @@
 package com.github.viqbgrg.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * 测试json Object 创建生成字符串
@@ -27,10 +26,17 @@ public class JSONStringTest {
         ObjectNode objectNode = objectMapper.createObjectNode();
         objectNode.put("name", "xiaoming");
         objectNode.put("age", 1);
-//        logger.info(objectNode.asText());
         log.info(objectNode.asText());
         log.info(objectNode.toString());
         log.info(objectNode.textValue());
         log.info(objectNode.toPrettyString());
+    }
+
+    @Test
+    void jsonArrayTest() {
+        ArrayNode arrayNode = objectMapper.createArrayNode();
+        arrayNode.add("23");
+        arrayNode.add("100");
+        log.info(arrayNode.toString());
     }
 }
