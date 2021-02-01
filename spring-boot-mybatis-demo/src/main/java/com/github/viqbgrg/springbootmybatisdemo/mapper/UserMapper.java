7f3b0491e218065dbfc21377bf9d8ad4ea8bd3e4
viqbgrg <viqbgrg@gmail.com>
2021-02-01 23:55:58 +0800

    mybaits user

--- spring-boot-mybatis-demo/src/main/java/com/github/viqbgrg/springbootmybatisdemo/mapper/UserMapper.java
+++ spring-boot-mybatis-demo/src/main/java/com/github/viqbgrg/springbootmybatisdemo/mapper/UserMapper.java
@@ -3,7 +3,9 @@ package com.github.viqbgrg.springbootmybatisdemo.mapper;
 import com.github.viqbgrg.springbootmybatisdemo.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
-    User selectAll();
+    List<User> selectAll();
 }
--- spring-boot-mybatis-demo/src/test/java/com/github/viqbgrg/springbootmybatisdemo/mapper/UserMapperTest.java
+++ spring-boot-mybatis-demo/src/test/java/com/github/viqbgrg/springbootmybatisdemo/mapper/UserMapperTest.java
@@ -1,15 +1,22 @@
 package com.github.viqbgrg.springbootmybatisdemo.mapper;
+import com.github.viqbgrg.springbootmybatisdemo.domain.User;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @MybatisTest
 public class UserMapperTest {
 
+    @Autowired
+    private UserMapper userMapper;
     /**
      * 把json数组转换成List
      */
     @Test
     void testJsonArrayToList() {
-
+        List<User> users = userMapper.selectAll();
+        System.out.println(users);
     }
 }
