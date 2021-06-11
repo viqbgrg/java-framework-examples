bc56e2ee6aec7ff1b2fbc244cc94bb89bb6472a2
viqbgrg <viqbgrg@gmail.com>
2021-06-11 13:21:17 +0800

    hello world

--- spring-native-demo/src/main/java/com/github/viqbgrg/springnativedemo/SpringNativeDemoApplication.java
+++ spring-native-demo/src/main/java/com/github/viqbgrg/springnativedemo/SpringNativeDemoApplication.java
@@ -2,12 +2,20 @@ package com.github.viqbgrg.springnativedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
+@RestController
 public class SpringNativeDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringNativeDemoApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
