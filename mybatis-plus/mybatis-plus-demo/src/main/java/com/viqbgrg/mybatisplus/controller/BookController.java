c458fc8cf0bc6e09413772f83de37a67c89ee682
viqbgrg <viqbgrg@gmail.com>
2021-06-22 22:19:04 +0800

    事务

--- mybatis-plus/mybatis-plus-demo/src/main/java/com/viqbgrg/mybatisplus/controller/BookController.java
+++ mybatis-plus/mybatis-plus-demo/src/main/java/com/viqbgrg/mybatisplus/controller/BookController.java
@@ -1,8 +1,16 @@
 package com.viqbgrg.mybatisplus.controller;
 
 
+import com.viqbgrg.mybatisplus.entity.Book;
+import com.viqbgrg.mybatisplus.service.IBookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -12,8 +20,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author bing
  * @since 2021-04-23
  */
-@Controller
+@RestController
 @RequestMapping("/book")
 public class BookController {
+    private IBookService bookService;
 
+    public BookController(IBookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("add")
+    public String add() {
+        ArrayList<Book> books = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Book book = new Book();
+            book.setId(Long.valueOf(i+20));
+            book.setName(String.valueOf(i+20));
+            books.add(book);
+        }
+        this.bookService.saveBatch(books);
+        Book book = new Book();
+        book.setId(Long.valueOf(31));
+        book.setName(String.valueOf(11));
+        this.bookService.save(book);
+        int a = 1/0;
+        System.out.println(a);
+        return "SUCCESS";
+    }
+
+    @GetMapping("add1")
+    @Transactional
+    public String add1() {
+        ArrayList<Book> books = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Book book = new Book();
+            book.setId(Long.valueOf(i+20));
+            book.setName(String.valueOf(i+20));
+            books.add(book);
+        }
+        this.bookService.saveBatch(books);
+        Book book = new Book();
+        book.setId(Long.valueOf(31));
+        book.setName(String.valueOf(11));
+        this.bookService.save(book);
+        int a = 1/0;
+        System.out.println(a);
+        return "SUCCESS";
+    }
+
+    @GetMapping("list")
+    public List<Book> list() {
+        return this.bookService.list();
+    }
 }
