2081de9021f4e9f8909841ca4635fb4e92aa7d6f
viqbgrg <viqbgrg@gmail.com>
2021-06-26 21:26:21 +0800

    排除项目中的datasources配置

--- shiro-demo/shiro-boot-demo/src/main/resources/data.sql
+++ shiro-demo/shiro-boot-demo/src/main/resources/data.sql
@@ -1,3 +1,7 @@
 insert into user(id, user_name, email, password, locked)
 values (1, '小明', '123456@qq.com', '$shiro1$SHA-256$500000$0pUW/XLY3ne7IuTjVsGWQA==$sl9v+Nk+UDdcqPb9JptPjk/gssiE3XPhWeJ91k0KYo0=', 0),
         (2, '小红', 'xiaohong@qq.com', '123456', 0);
+
+insert into roles(id, role, description, available)
+values ( 1, 'manager', '经理', 1 );
+
