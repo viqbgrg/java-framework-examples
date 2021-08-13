a13f0c31e9e7cfa571ea0d6c5b6baf98d9ccb1dc
viqbgrg <viqbgrg@gmail.com>
2021-08-13 14:37:30 +0800

    不设置名字拦截器顺序会靠前

--- shiro-demo/shiro-boot-mvc-filter-demo/src/main/java/com/github/viqbgrg/shiro/boot/mvc/filter/demo/ShiroConfig.java
+++ shiro-demo/shiro-boot-mvc-filter-demo/src/main/java/com/github/viqbgrg/shiro/boot/mvc/filter/demo/ShiroConfig.java
@@ -2,11 +2,16 @@ package com.github.viqbgrg.shiro.boot.mvc.filter.demo;
 
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+import org.apache.shiro.web.servlet.AbstractShiroFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
 
 /**
  * @author hhj
@@ -23,6 +28,7 @@ public class ShiroConfig {
         return new SimpleAccountRealm();
     }
 
+    public static final String FILTER_NAME = "shiroFilter";
     @Bean
     public ShiroFilterChainDefinition shiroFilterChainDefinition() {
         DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
@@ -31,5 +37,16 @@ public class ShiroConfig {
         chainDefinition.addPathDefinition("/**", "shiroFilter");
 
         return chainDefinition;
+    }
+
+    @Bean
+    protected FilterRegistrationBean filterShiroFilterRegistrationBean(ShiroFilterFactoryBean shiroFilterFactoryBean) throws Exception {
+        FilterRegistrationBean<AbstractShiroFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC);
+        filterRegistrationBean.setFilter((AbstractShiroFilter) shiroFilterFactoryBean.getObject());
+        filterRegistrationBean.setName(FILTER_NAME);
+        filterRegistrationBean.setOrder(1);
+
+        return filterRegistrationBean;
     }
 }
