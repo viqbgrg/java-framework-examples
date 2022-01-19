13295db14d5e76010cc439b15ecc8e6006d09d2e
viqbgrg <viqbgrg@gmail.com>
2022-01-19 16:17:42 +0800

    共享变量

---
+++ spock-unittest-demo/spock-unittest/src/main/java/com/github/viqbgrg/spockunittestdemo/ClassA.java
@@ -0,0 +1,11 @@
+package com.github.viqbgrg.spockunittestdemo;
+
+public class ClassA {
+    public ClassA() {
+        System.out.println("ClassA 初始化");
+    }
+
+    public void print(int a){
+        System.out.println("第" + a + "次执行!");
+    }
+}
