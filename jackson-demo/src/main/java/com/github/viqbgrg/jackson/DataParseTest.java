0d48626453d0848091ad2319f0eace471a9cd8e3
viqbgrg <viqbgrg@gmail.com>
2022-02-18 18:45:17 +0800

    时间格式化

--- jackson-demo/src/main/java/com/github/viqbgrg/jackson/DataParseTest.java
+++ jackson-demo/src/main/java/com/github/viqbgrg/jackson/DataParseTest.java
@@ -2,6 +2,7 @@ package com.github.viqbgrg.jackson;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -10,6 +11,7 @@ import lombok.Data;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class DataParseTest {
 
@@ -21,6 +23,19 @@ public class DataParseTest {
                 .build();
         MyData myData = objectMapper.readValue(json, MyData.class);
         System.out.println(myData.getData().toString());
+    }
+
+    @Test
+    public void dataListTest() throws JsonProcessingException {
+        String json= "[{\"data\": \"2022-02-15T11:31:43.509Z\"}]";
+        ObjectMapper objectMapper = JsonMapper.builder()
+                .findAndAddModules()
+                .build();
+        TypeReference<List<MyData>> typeRef
+                = new TypeReference<>() {
+        };
+        List<MyData> myData = objectMapper.readValue(json, typeRef);
+        System.out.println(myData.get(0).getData().toString());
     }
 
     @Data
