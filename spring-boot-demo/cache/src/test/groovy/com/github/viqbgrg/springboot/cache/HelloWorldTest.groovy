a234706b775d25c1135ceb552ac8d70dec66232c
viqbgrg <viqbgrg@gmail.com>
2022-09-15 14:39:34 +0800

    手动操作一些东西

--- spring-boot-demo/cache/src/test/groovy/com/github/viqbgrg/springboot/cache/HelloWorldTest.groovy
+++ spring-boot-demo/cache/src/test/groovy/com/github/viqbgrg/springboot/cache/HelloWorldTest.groovy
@@ -6,6 +6,8 @@ import org.spockframework.spring.SpringSpy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
+import javax.cache.CacheManager
+
 @SpringBootTest
 class HelloWorldTest extends spock.lang.Specification {
 
@@ -14,6 +16,9 @@ class HelloWorldTest extends spock.lang.Specification {
 
     @Autowired
     HelloWorld helloWorld
+
+    @Autowired
+    CacheManager cacheManager
 
 
     def longToString() {
@@ -24,5 +29,15 @@ class HelloWorldTest extends spock.lang.Specification {
         helloWorld.longToString(value)
         then:
         0 * dataService.longToString(value)
+    }
+
+    def cacheManagerTest() {
+        expect:
+        def cache = cacheManager.getCache("test", Long.class, String.class)
+        def get = cache.get(1L)
+        get != null
+        def list = cache.asList()
+        def value = list.get(0).getValue()
+        value != null
     }
 }
