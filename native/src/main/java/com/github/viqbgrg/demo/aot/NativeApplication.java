ed8cb1c402112326a0fe253d79e70c02fc00370a
viqbgrg <viqbgrg@gmail.com>
2022-12-28 15:51:29 +0800

    aot

--- native/src/main/java/com/github/viqbgrg/demo/aot/NativeApplication.java
+++ native/src/main/java/com/github/viqbgrg/demo/aot/NativeApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.github.viqbgrg.demo.aot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
