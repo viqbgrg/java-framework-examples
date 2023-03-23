fb19f0064422a4349b8924636b3d406dd04bc4e4
viqbgrg <viqbgrg@gmail.com>
2023-03-23 15:05:58 +0800

    spring doc

---
+++ spingdoc-demo/src/main/java/com/github/viqbgrg/spingdocdemo/SpingdocDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.spingdocdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingdocDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingdocDemoApplication.class, args);
+	}
+
+}
---
+++ spingdoc-demo/src/test/java/com/github/viqbgrg/spingdocdemo/SpingdocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.spingdocdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingdocDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
