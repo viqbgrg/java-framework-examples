f65b9b0cce3302dbbe509bd61c3a0bf514b8560a
viqbgrg <viqbgrg@gmail.com>
2023-03-29 17:06:29 +0800

    useJUnitPlatform

--- spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/AComponentTest.groovy
+++ spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/AComponentTest.groovy
@@ -1,6 +1,8 @@
 package com.github.viqbgrg.testing.context
 
-class AComponentTest extends spock.lang.Specification {
+import spock.lang.Specification
+
+class AComponentTest extends Specification {
     def "bean test"() {
         expect:
         print("111")
