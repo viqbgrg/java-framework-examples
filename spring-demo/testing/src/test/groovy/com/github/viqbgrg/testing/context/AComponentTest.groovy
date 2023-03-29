b6aa800861f7636dd8af3d92884e3132aa1448fa
viqbgrg <viqbgrg@gmail.com>
2023-03-29 16:19:13 +0800

    spring test

--- spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/AComponentTest.groovy
+++ spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/AComponentTest.groovy
@@ -1,4 +1,8 @@
 package com.github.viqbgrg.testing.context
 
 class AComponentTest extends spock.lang.Specification {
+    def "bean test"() {
+        expect:
+        print("111")
+    }
 }
---
+++ spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/Test.groovy
@@ -0,0 +1,11 @@
+package com.github.viqbgrg.testing.context
+
+import spock.lang.Specification
+
+class Test extends Specification{
+
+    def "test"() {
+        expect:
+        1 == 1
+    }
+}
