4cdc8987624bf0da980af498233d78c476f4bd9d
viqbgrg <viqbgrg@gmail.com>
2023-03-29 17:26:29 +0800

    ContextConfiguration

--- spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/AComponentTest.groovy
+++ spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/AComponentTest.groovy
@@ -1,10 +1,20 @@
 package com.github.viqbgrg.testing.context
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
+@ContextConfiguration(classes = AComponent.class)
 class AComponentTest extends Specification {
+    @Autowired
+    ApplicationContext applicationContext
     def "bean test"() {
+        given:
+        def bean = applicationContext.getBean(AComponent.class)
+        def bComponent = applicationContext.getBean(BComponent.class)
         expect:
-        print("111")
+        bean != null
+        bComponent == null
     }
 }
--- spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/RuntimeCompatibilitySpec.groovy
+++ spring-demo/testing/src/test/groovy/com/github/viqbgrg/testing/context/RuntimeCompatibilitySpec.groovy
@@ -1,5 +1,6 @@
 package com.github.viqbgrg.testing.context
 
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
