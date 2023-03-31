e70bd6f150f87179db30fe5cf9c27a9681bf4e8f
viqbgrg <viqbgrg@gmail.com>
2023-03-31 19:43:19 +0800

    任务结束关闭 cache

--- ehcache-demo/basic/src/test/groovy/com/github/viqbgrg/demo/ehcache/BasicXMLTest.groovy
+++ ehcache-demo/basic/src/test/groovy/com/github/viqbgrg/demo/ehcache/BasicXMLTest.groovy
@@ -1,17 +1,37 @@
 package com.github.viqbgrg.demo.ehcache
 
+import org.ehcache.Cache
+import org.ehcache.CacheManager
+import spock.lang.Shared
 import spock.lang.Specification
 
 class BasicXMLTest extends Specification {
 
+    @Shared CacheManager manager
 
+    def setupSpec() {
+        def l = new BasicXML()
+        manager = l.getCacheManager()
+    }
 
     def "GetCacheManager"() {
-        given:
-        def l = new BasicXML()
-        when:
-        def manager = l.getCacheManager()
-        then:
+        expect:
         manager != null
+    }
+
+    def "arrayListTest"() {
+        given:
+        def cache = manager.getCache("listCache", String.class, String.class)
+        when:
+        Arrays.asList("a", "b")
+        cache.put("test", "b")
+        then:
+        def get = cache.get("test")
+        get != null
+        get == "b"
+    }
+
+    def cleanupSpec() {
+        manager.close()
     }
 }
