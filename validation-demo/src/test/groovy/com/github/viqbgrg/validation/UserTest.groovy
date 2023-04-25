27973c5c413ce10eef71632a541b43fa12e4bd49
viqbgrg <viqbgrg@gmail.com>
2023-04-25 20:09:53 +0800

    update gradle

--- validation-demo/src/test/groovy/com/github/viqbgrg/validation/UserTest.groovy
+++ validation-demo/src/test/groovy/com/github/viqbgrg/validation/UserTest.groovy
@@ -1,5 +1,9 @@
 package com.github.viqbgrg.validation
 
+import jakarta.validation.ConstraintViolation
+import jakarta.validation.Validation
+import jakarta.validation.Validator
+import jakarta.validation.ValidatorFactory
 import spock.lang.Specification
 
 class UserTest extends Specification {
@@ -8,5 +12,13 @@ class UserTest extends Specification {
         expect:
         def user = new User()
         user.setUsername(null)
+        // 1、使用【默认配置】得到一个校验工厂  这个配置可以来自于provider、SPI提供
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        // 2、得到一个校验器
+        Validator validator = validatorFactory.getValidator()
+        // 3、校验Java Bean（解析注解） 返回校验结果
+        Set<ConstraintViolation<User>> result = validator.validate(user)
+        // 输出校验结果
+        result.stream().map(v -> v.getPropertyPath() + " " + v.getMessage() + ": " + v.getInvalidValue()).forEach(System.out::println);
     }
 }
