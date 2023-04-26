8ac0259561b0882b73145df6ada1bfaa6a4211ec
viqbgrg <viqbgrg@gmail.com>
2023-04-26 10:42:25 +0800

    validate

--- validation-demo/src/main/java/com/github/viqbgrg/validation/User.java
+++ validation-demo/src/main/java/com/github/viqbgrg/validation/User.java
@@ -3,6 +3,7 @@ package com.github.viqbgrg.validation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
@@ -10,10 +11,15 @@ import lombok.Data;
  * @author hhj
  */
 @Data
+@AllArgsConstructor
 public class User {
-    @NotEmpty(message = "不能为empty")
+    /**
+     * empty 空字符串 长度为 0
+     * blank 空格字符串
+     */
+    @NotEmpty(message = "不能为 empty")
     private String emptyString;
-    @NotNull(message = "不能为null")
+    @NotNull(message = "不能为 null")
     private String nullString;
     @NotBlank(message = "不能为 blank")
     private String blankString;
--- validation-demo/src/test/groovy/com/github/viqbgrg/validation/utils/ValidateUtilsTest.groovy
+++ validation-demo/src/test/groovy/com/github/viqbgrg/validation/utils/ValidateUtilsTest.groovy
@@ -1,6 +1,22 @@
 package com.github.viqbgrg.validation.utils
 
+import com.github.viqbgrg.validation.User
 import spock.lang.Specification
 
 class ValidateUtilsTest extends Specification {
+    def "validate"() {
+        expect:
+        def validate = ValidateUtils.getValidate(user)
+        validate.forEach { it ->
+            {
+                println it.getMessage()
+            }
+        }
+        validate.size() == size
+        where:
+        user                    | size
+        new User(" ", null, "") | 2
+        new User("", null, " ") | 3
+        new User(null, null, " ") | 3
+    }
 }
