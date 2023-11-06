b8fe178b47c834d7341bf4afad8c2708d63117b3
viqbgrg <viqbgrg@gmail.com>
2023-11-06 23:56:13 +0800

    WebMethod

--- spring-boot-demo/webservice/src/main/java/com/github/viqbgrg/springboot/service/HelloWorld.java
+++ spring-boot-demo/webservice/src/main/java/com/github/viqbgrg/springboot/service/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.github.viqbgrg.springboot.service;
 
 
+import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 
 @WebService
 public interface HelloWorld {
+    @WebMethod(operationName = "getHello", action = "bbbb")
     String sayHi(String text);
 }
--- spring-boot-demo/webservice/src/main/java/com/github/viqbgrg/springboot/service/impl/HelloWorldImpl.java
+++ spring-boot-demo/webservice/src/main/java/com/github/viqbgrg/springboot/service/impl/HelloWorldImpl.java
@@ -6,6 +6,7 @@ import jakarta.jws.WebService;
 @WebService(endpointInterface = "com.github.viqbgrg.springboot.service.HelloWorld")
 public class HelloWorldImpl implements HelloWorld {
 
+
     public String sayHi(String text) {
         System.out.println("sayHi called");
         return "Hello " + text;
